d4507fc0d6eb2755514306fcc0352e5c5b92ec00
Danail Gatev <danailgatev11@gmail.com>
2025-10-24 20:34:47 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+<script>
+function toggleDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.hamburger')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+</script>
